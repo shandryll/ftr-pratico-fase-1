1c71464f921eb3994cb7e8942773b893b7801a9d
shandryll <shandryll94@gmail.com>
2025-04-08 09:45:18 -0300

    feat: initial frontend config

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <>
+      <div>Hello World</div>
+    </>
+  )
+}
+
+export default App
---
+++ web/tailwind.config.js
@@ -0,0 +1,29 @@
+import { theme } from 'tailwindcss/defaultConfig'
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans', ...theme.fontFamily.sans],
+      },
+      fontSize: {
+        xs: ["0.625rem", { lineHeight: "0.875rem" }],
+        sm: ["0.75rem", { lineHeight: "1rem" }],
+        base: ["0.875rem", { lineHeight: "1.125rem" }],
+        lg: ["1.125rem", { lineHeight: "1.5rem" }],
+        xl: ["1.5rem", { lineHeight: "2rem" }],
+      },
+      colors: {
+        "blue-base": "#2C46B1",
+        "blue-dark": "#2C4091",
+        "danger": "#B12C4D",
+      },
+    },
+  },
+  plugins: [],
+}
