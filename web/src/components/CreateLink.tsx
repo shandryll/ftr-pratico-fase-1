778e963f3ea5431de325c9a8fd4bbee3907736ab
shandryll <shandryll94@gmail.com>
2025-04-15 17:05:24 -0300

    feat: edit components height and weight

--- web/src/components/CreateLink.tsx
+++ web/src/components/CreateLink.tsx
@@ -3,7 +3,7 @@ import TextInput from "./ui/TextInput";
 
 export function CreateLink() {
   return (
-    <div className="bg-gray-100 flex flex-col p-[2rem] rounded-xl w-[380px]">
+    <div className="bg-gray-100 flex flex-col p-[2rem] rounded-xl w-[380px] max-h-[360px]">
 
       <div className="pb-[1.5rem]">
         <h1 className="text-lg text-gray-600 font-bold">Novo link</h1>
