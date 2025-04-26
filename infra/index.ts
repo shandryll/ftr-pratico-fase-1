30e8ad93edd9a8c6c029552c25989c34cdff542d
shandryll <shandryll94@gmail.com>
2025-04-26 02:38:27 -0300

    feat: create pulumi deploy and preview pipelines

--- infra/index.ts
+++ infra/index.ts
@@ -1,6 +1,6 @@
 import * as aws from "@pulumi/aws";
 
-const bucket = new aws.s3.BucketV2("brev-lv", {
+const brevly_s3 = new aws.s3.BucketV2("brev-lv", {
   bucket: 'brev-lv',
   tags: {
     IAC: "true"
