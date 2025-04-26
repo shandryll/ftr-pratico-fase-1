b50d4e8b2b4f9fb621756fb987419ad3e441fbea
shandryll <shandryll94@gmail.com>
2025-04-26 00:21:54 -0300

    feat: configure infra data

--- infra/index.ts
+++ infra/index.ts
@@ -1,9 +1,11 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2("my-bucket");
+const bucket = new aws.s3.BucketV2("brev-lv", {
+  bucket: 'brev-lv',
+  tags: {
+    IAC: "true"
+  }
+});
 
-// Export the name of the bucket
 export const bucketName = bucket.id;
+export const bucketInfo = bucket.bucket;
