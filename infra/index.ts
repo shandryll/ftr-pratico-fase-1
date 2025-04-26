04c251adf3ded25a13eb39a9a35921c0a97380fb
shandryll <shandryll94@gmail.com>
2025-04-26 02:29:34 -0300

    fix: resolve infra workflow

--- infra/index.ts
+++ infra/index.ts
@@ -1,21 +1,7 @@
 import * as aws from "@pulumi/aws";
 
-const bucket = new aws.s3.BucketV2("brev-lv", {
+const brevly_s3 = new aws.s3.BucketV2("brev-lv", {
   bucket: 'brev-lv',
-  tags: {
-    IAC: "true"
-  }
-});
-
-const bkpsss = new aws.s3.BucketV2("bkpsss-brev-lv", {
-  bucket: 'bkpsss-brev-lv',
-  tags: {
-    IAC: "true"
-  }
-});
-
-const bkp = new aws.s3.BucketV2("bkp-brev-lv", {
-  bucket: 'bkp-brev-lv',
   tags: {
     IAC: "true"
   }
